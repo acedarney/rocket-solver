74888fc0429752a946d99d2c80b6f4b70e427ece
Dale Arney <acedarney@gmail.com>
2018-03-05 21:27:08 -0500

    Refactor the calculations of propellant and inert mass, write to CSV

--- rocket.py
+++ rocket.py
@@ -1,43 +1,37 @@
 import numpy as np
 import itertools as it
 import pandas as pd
+from numba import vectorize, float64
+from time import time
 
 
-def calc_prop(m_inert, m_payload, mass_ratio):
-    m_prop = (m_inert + m_payload) * (mass_ratio - 1)
+@vectorize([float64(float64, float64, float64)])
+def calc_prop(m_payload, mass_ratio, inert_mass_frac):
+    m_prop = m_payload * (mass_ratio - 1) * (1 - inert_mass_frac) / (1 - mass_ratio * inert_mass_frac)
     return m_prop
 
 
-def calc_inert(m_prop, inert_mass_frac):
-    m_inert = m_prop / (1 - inert_mass_frac)
+@vectorize([float64(float64, float64, float64)])
+def calc_inert(m_payload, mass_ratio, inert_mass_frac):
+    m_inert = m_payload * (mass_ratio - 1) * inert_mass_frac / (1 - mass_ratio * inert_mass_frac)
     return m_inert
 
 
-# Replace iterative convergence with explicit equations (it's all IMF sizing for now)
-@np.vectorize
-def converge(delta_v, m_payload, inert_mass_frac, specific_impulse):
-    mass_ratio = np.exp(delta_v / 9.80665 / specific_impulse)
-    err = 100
-    m_inert_guess = 1000.
-    while err > 1:
-        m_prop = calc_prop(m_inert_guess, m_payload, mass_ratio)
-        m_inert_actual = calc_inert(m_prop, inert_mass_frac)
-        err = np.abs(m_inert_actual - m_inert_guess)
-        m_inert_guess = m_inert_actual
-    m_inert = m_inert_actual
-    return m_inert, m_prop
-
-
 if __name__ == '__main__':
-    delta_v_range = np.arange(2000, 2005, 5)
+    tic = time()
+    delta_v_range = np.arange(2000, 3005, 5)
     # print(delta_v_range[0:5])
-    imf_range = np.arange(0.15, 0.16, 0.01)
+    imf_range = np.arange(0.15, 0.26, 0.01)
     # print(imf_range[0:5])
-    m_payload_range = np.arange(3000, 3010, 10)
+    m_payload_range = np.arange(3000, 4010, 10)
     # print(m_payload_range[0:5])
     a = it.product(*[delta_v_range, imf_range, m_payload_range])
     df = pd.DataFrame(list(a), columns=['Delta V (m/s)', 'IMF', 'Payload Mass (kg)'])
     df['Isp (s)'] = 325
-    df['Inert Mass (kg)'], df['Propellant Mass (kg)'] = converge(df['Delta V (m/s)'], df['Payload Mass (kg)'],
-                                                                 df['IMF'], df['Isp (s)'])
+    df['Mass Ratio'] = np.exp(df['Delta V (m/s)'] / 9.80665 / df['Isp (s)'])
+    df['Inert Mass (kg)'] = calc_inert(df['Payload Mass (kg)'], df['Mass Ratio'], df['IMF'])
+    df['Propellant Mass (kg)'] = calc_prop(df['Payload Mass (kg)'], df['Mass Ratio'], df['IMF'])
+    df.to_csv('propulsive_stage_data.csv')
+    toc = time()
+    print('Elapsed Time: {}'.format(toc - tic))
     print(df.head())
