9f420233af61c12713568421bc5d73633014ebcd
Dale Arney <acedarney@gmail.com>
2018-03-25 09:54:09 -0400

    Refactored the blog urls.py (still non functional)

--- prologaero/blog/urls.py
+++ prologaero/blog/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
-from .models import Post
-from .views import PostDetailView
+from django.urls import path
+from .views import PostDetailView, PostListView, index
 
 urlpatterns = [
-    url('', ListView.as_view(queryset=Post.objects.all().order_by('-date')[:25],
-                             template_name='blog/blog.html'), name='blog'),
-    url('<int:pk>/', PostDetailView.as_view(), name='post')
+    path('', index, name='blog-index'),
+    url('posts/', PostListView.as_view(), name='blog'),
+    url('post/<int:pk>/', PostDetailView.as_view(), name='post')
 ]
--- prologaero/blog/views.py
+++ prologaero/blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Post
+
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/header_footer.html')
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post.html'
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+    # TODO: Sort the blog posts in descending order
